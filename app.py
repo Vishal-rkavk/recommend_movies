5c35bb66a61636b369099867b0c7882ea2467258
vishal-rkavk <vikkireddy321@gmail.com>
2023-02-18 19:13:36 +0100

    update

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask,render_template
+import pandas as pd
+import os
+
+from config.config_files import APIkeys
+
+api_key = APIkeys.api_key
 
 
+def get_movie_suggestions():
+    pass
 
 
 app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    suggestions = get_movie_suggestions()
+    return render_template('index.html',suggestions=suggestions)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
